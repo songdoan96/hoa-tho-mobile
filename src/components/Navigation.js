5849db3d2b8904b872669f6ce190e3363f91ba3f
Song Doan <songdoan96@gmail.com>
2023-12-17 14:14:39 +0700

    navigation and redux

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,23 +6,35 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const user = false;
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Home"
-          options={{headerShown: false}}
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{headerShown: false}}
-          component={LoginScreen}
-        />
+      <Stack.Navigator initialRouteName="Home">
+        {user ? (
+          <Stack.Screen
+            name="Home"
+            options={{headerShown: false}}
+            component={HomeScreen}
+          />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              options={{headerShown: false}}
+              component={LoginScreen}
+            />
+            <Stack.Screen
+              name="Register"
+              options={{headerShown: false}}
+              component={RegisterScreen}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import {configureStore} from '@reduxjs/toolkit';
-// import {authSlice} from './authSlice';
 import authReducer from './AuthSlice';
 
 export const store = configureStore({
